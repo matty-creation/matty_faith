6dfe7dc794b4d2b20acd88f0f3b7dc6950d37f50
matty-creation <matildamwaisemba95@gmail.com>
2026-08-28 08:27:44 +0300

    hii

--- back_end/app/main.py
+++ back_end/app/main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 from app.routers.employees import router as employee_router
 from app.routers.department import router as department_router
 from app.routers.attendance import router as attendance_router
+from app.routers.auth import router as auth_router
 
 app = FastAPI()
 
 app.include_router(employee_router)
 app.include_router(department_router)
 app.include_router(attendance_router)
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
---
+++ back_end/app/routers/auth.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+
+from app.database.session import get_session
+from app.models.employee import Employee
+from app.schemas.auth import LoginRequest
+from app.utils.security import verify_password
+
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+
+@router.post("/login")
+def login(
+    login_data: LoginRequest,
+    session: Session = Depends(get_session)
+):
+    employee = session.exec(
+        select(Employee).where(
+            Employee.email == login_data.email
+        )
+    ).first()
+
+    if not employee:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(
+        login_data.password,
+        employee.password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    return {
+        "message": "Login successful",
+        "employee_id": employee.employee_id
+    }
